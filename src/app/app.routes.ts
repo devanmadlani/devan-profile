46f51cee787ca57a94ec2fcccdaffe700b54ba9e
Devan Madlani <d.madlani@linx-it.nl>
2024-01-29 19:31:02 +0100

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './profile/home/home.component';
+import { WorkComponent } from './profile/work/work.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProfileComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'work', component: WorkComponent },
+    ],
   },
 ];
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
