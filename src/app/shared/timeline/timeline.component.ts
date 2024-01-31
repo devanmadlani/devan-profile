11cd9166e3c4cbf2d6074e47f528affe8686b40f
Devan Madlani <d.madlani@linx-it.nl>
2024-01-31 16:53:06 +0100

    fix

--- src/app/shared/timeline/timeline.component.ts
+++ src/app/shared/timeline/timeline.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-timeline',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './timeline.component.html',
-  styleUrl: './timeline.component.scss'
+  styleUrl: './timeline.component.scss',
 })
-export class TimelineComponent {
-
-}
+export class TimelineComponent {}
